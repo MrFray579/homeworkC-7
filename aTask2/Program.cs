8ce7035cf34198b91e550c7d8a65da77345bb8ec
Fray <kozar20000@mail.ru>
2022-07-19 18:16:23 +0300

    Element

---
+++ aTask2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите позицию элемента:");
+int i0 = Convert.ToInt32(Console.ReadLine());
+int j0 = Convert.ToInt32(Console.ReadLine());
+
+
+
+int[,] array = new int[M, N];
+
+Random rnd = new Random();
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+    array[i, j] = rnd.Next(0, 10);
+    Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+if (i0 >= M && j0 >= N || i0 < 0 || j0 < 0)
+Console.WriteLine("Такого числа в массиве нет.");
+else
+Console.WriteLine("Элемент {0}", array[i0, j0]);
