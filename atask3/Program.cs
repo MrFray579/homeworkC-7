78c64d8bd3842a92a685cf814d9eb80abc67bf16
Fray <kozar20000@mail.ru>
2022-07-19 18:35:47 +0300

    task3

---
+++ atask3/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое число:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[M, N];
+
+Random rnd = new Random();
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+    array[i, j] = rnd.Next(0, 10);
+    Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < N; j++)
+{
+    double sum = 0;
+    for (int i = 0; i < M; i++)
+    {
+        sum += array[i, j];
+    }
+    Console.WriteLine("Sum is {0}", sum / M);
+}
+
